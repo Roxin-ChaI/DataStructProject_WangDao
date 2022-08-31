1e83f529fa209a832a5d6e3cd0191764f0b3e78a
Roxin-ChaI <68932827+Roxin-ChaI@users.noreply.github.com>
2022-08-31 16:54:40 +0800

    增加注释
    写注释是个好习惯，一定要写注释！

--- 线性表/P18-1.cpp
+++ 线性表/P18-1.cpp
@@ -8,18 +8,18 @@ typedef struct  sqlist
 
 bool deletelist(sqlist &s ,int &e){
 	if(s.length == 0){
-		return 0;
+		return 0;//表空，终止并返回
 	}
 	e=s.a[0];
-	int flag =0;
-	for(int i=1;i<s.length;i++)
-		if(s.a[i] < e){
-			e=s.a[i];
-			flag=i;
+	int flag =0;//flag指向0号元素
+	for(int i=1;i<s.length;i++)//从1号元素开始向后遍历
+		if(s.a[i] < e){//若后一位的元素的值小于e
+			e=s.a[i];//则赋值于e
+			flag=i;//flag指向第i号元素
 		}
-	s.a[flag]=s.a[s.length - 1];
-	s.length--;
-	return 1;
+	s.a[flag]=s.a[s.length - 1];//最小值所在位置赋值为最后一位
+	s.length--;//长度减一
+	return 1;//此时e是最小值
 }
 
 int main()
